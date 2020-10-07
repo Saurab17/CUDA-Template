d18dbe50b1f51447b7d8d80219dbe95a21a6bb10
saurabh <saurabh.yadavx@gmail.com>
2020-10-07 14:12:16 +0530

    finished hello world program

---
+++ hello_world.cu
@@ -0,0 +1,29 @@
+/* ---------------------------------------------------
+   My Hello world for CUDA programming
+   --------------------------------------------------- */
+
+#include <stdio.h>        
+#include <unistd.h>  
+     
+#include <cuda_runtime.h>
+
+/* ------------------------------------
+   Kernel (GPU function)
+   ------------------------------------ */
+__global__ void hello(void)
+{
+   printf("Hello From the GPU !\n");
+}
+
+int main()
+{
+   /* ------------------------------------
+      Call to the hello( ) kernel function
+      ------------------------------------ */
+    hello<<<1, 1>>>();
+
+    printf("Hello From the CPU ! \n");
+    
+    cudaDeviceSynchronize();
+    return 0;
+}
